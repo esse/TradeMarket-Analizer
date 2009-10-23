89ff204a8e811eb1366fbfe3b9e4276ba1ced4dd
Grzegorz Wieczorek <grzewster@gmail.com>
2009-10-23 14:38:11 +0200

    wersja 1.0 RC

---
+++ parser-wiki/cfg.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+###
+# Grzegorz Wieczorek 07.09.2009 00:43
+# gg: 5905482
+# jabb: grzew@jabster.pl
+# email: grzewster@gmail.com
+# 
+###
+
+## Dane do bazy danych:
+
+def dane():
+    login = "root"     # login do bazy danych
+    haslo = "dupa"     # haslo do bazy danych
+    host = "localhost" # adres pod ktorym znajduje sie baza
+    baza = "parser"    # nazwa bazy danych
+
+    ## Nic nie zmieniac ponizej tej linii
+
+    return login,haslo,host,baza
+
+    
